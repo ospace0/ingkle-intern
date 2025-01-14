49908db43d4faa57ed2b43be68d5f89405337dd6
Yonggiene <dlfnek2002@naver.com>
2025-01-14 17:59:14 +0900

    url_key_data etc

--- url2parquet/url2parquet_url.py
+++ url2parquet/url2parquet_url.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 from glob import glob
 from io import BytesIO
 from nc_read_url import nc_reader
-
+from key_data_date_path import auth_key, data_types, region, start_date, end_date, parquet_directory, merged_directory 
 
 # 이미지 데이터를 Parquet로 저장하는 함수
 def process_and_save_parquet(data_list, data_types, parquet_save_path, size_ranges):
@@ -121,13 +121,6 @@ def merge_parquet_files(input_directory, output_directory):
 def main():
     start_time = time.time()  # 시작 시간 기록
 
-    auth_key = "6vdMscAHSSC3TLHABykgvw"
-    data_types = ["VI004", "VI005", "VI006", "VI008", "NR013", "NR016", "SW038", "WV063", "WV069", "WV073", "IR087", "IR096", "IR105", "IR112", "IR123", "IR133"]
-    region = "KO"
-    start_date = datetime(2025, 1, 10)  # 검색 시작일
-    end_date = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
-    parquet_directory = "D:/sat_file/temp_parquet_hourly"  # 임시 시간별 parquet파일 저장 경로
-    merged_directory = "D:/sat_file/daily_parquets"  # 일별 parquet files
     os.makedirs(parquet_directory, exist_ok=True)
 
     # 필요한 크기와 범위 정의
